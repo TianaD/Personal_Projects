2c83e6dc5ccb87ff9d050500def7fd8213cfa69c
TianaD <tldavis99@yahoo.com>
2023-06-20 14:09:36 -0500

    Tic-Tac-Toe CommitBeforeIloseIt

---
+++ tic-tac-toe/restart.js
@@ -0,0 +1,16 @@
+let restartBtn = document.getElementById('restartBtn')
+
+restartBtn.addEventListener('click', restart)
+
+function restart() {
+    spaces.fill(null)
+
+    boxes.forEach( box => {
+        box.innerText = ''
+        box.style.backgroundColor=''
+    })
+
+    playerText.innerHTML = 'Tic Tac Toe'
+
+    currentPlayer = X_text
+}
